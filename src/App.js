0d133aa36c5676aaa4ce63f0efff6d7e57e0a1d4
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-27 00:22:59 +0200

    Add input from to fetch different city weather

--- src/App.js
+++ src/App.js
@@ -35,6 +35,24 @@ class App extends Component {
     window.M.Modal.init(elems);
   }
 
+  searchCity = (e) => {
+    e.preventDefault();
+    const city = document.getElementById("city").value;
+    console.log(city);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d`;
+
+    axios.get(url).then((resp) => {
+      this.setState({
+        cityName: resp.data.name,
+        temp: resp.data.main.temp,
+        high: resp.data.main.temp_max,
+        low: resp.data.main.temp_min,
+        weather: resp.data.weather[0].description,
+        icon: resp.data.weather[0].icon,
+      });
+    });
+  };
+
   render() {
     const iconUrl = `http://openweathermap.org/img/wn/${this.state.icon}@2x.png`;
 
@@ -54,16 +72,27 @@ class App extends Component {
 
         <div className="modal-fetch">
           {/* Modal Trigger */}
-          <a
-            className="waves-effect waves-light btn modal-trigger"
-            href="#modal1">
-            {this.state.cityName}
-          </a>
+          <div className="row">
+            <div className="col s6 offset-s3">
+              <h3>{this.state.cityName}</h3>
+              <a
+                className="waves-effect waves-light btn modal-trigger"
+                href="#modal1">
+                Details
+              </a>
+              <form onSubmit={this.searchCity}>
+                <input type="text" id="city" placeholder="Enter a city name" />
+              </form>
+            </div>
+          </div>
 
           {/* Modal Structure */}
           <div id="modal1" className="modal">
             <div className="modal-content">
-              <h4>{this.state.temp} Fahrenheit</h4>
+              <h4>{this.state.cityName}</h4>
+              <p>
+                <b>{this.state.temp} Fahrenheit</b>
+              </p>
               <p>
                 High: {this.state.high} | Low: {this.state.low}
               </p>
