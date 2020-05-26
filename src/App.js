c577de9345e172e7004cff52d3517428293d6535
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-26 18:37:55 +0200

    Fetch temperature from api and mount it in DOM

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import axios from "axios";
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      temp: "",
+    };
+  }
 
+  componentDidMount() {
     // TODO: Change API key to my own
     const url =
       "https://api.openweathermap.org/data/2.5/weather?q=Berlin&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d";
 
     axios.get(url).then((resp) => {
-      console.log(resp.data);
+      this.setState({ temp: resp.data.main.temp });
     });
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Sanity Check</h1>
+        <h1>The Component Lifecycle and HTTP</h1>
+        <p>
+          Current weather in Berlin is <b>{this.state.temp}</b> Fahrenheit
+        </p>
       </div>
     );
   }
