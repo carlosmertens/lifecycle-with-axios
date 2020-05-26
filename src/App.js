ab90f642115c80a1cd4758c1101e064d358aa29d
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-26 16:07:53 +0200

    Convert stateless function App to class component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
 
-function App() {
-  // TODO: Change to own API key
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Berlin&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d";
+class App extends Component {
+  constructor() {
+    super();
 
-  axios.get(url).then((resp) => {
-    console.log(resp.data);
-  });
+    // TODO: Change API key to my own
+    const url =
+      "https://api.openweathermap.org/data/2.5/weather?q=Berlin&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d";
 
-  return (
-    <div className="App">
-      <h1>Sanity Check</h1>
-    </div>
-  );
+    axios.get(url).then((resp) => {
+      console.log(resp.data);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Sanity Check</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
