dc0bae1dbdcc9f966c77386874849381efc94106
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-26 15:55:01 +0200

    Test axios with openweathermap.org api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  // TODO: Change to own API key
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Berlin&units=imperial&appid=e312dbeb8840e51f92334498a261ca1d";
+
+  axios.get(url).then((resp) => {
+    console.log(resp.data);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sanity Check</h1>
     </div>
   );
 }
