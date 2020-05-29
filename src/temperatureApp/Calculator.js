e541c444b46160d4a039c92752e74db186bd750a
Carlos Mertens <mertenscarlos@yahoo.com>
2020-05-29 11:50:39 +0200

    Implement celsius to fahrenheit convertion

--- src/temperatureApp/Calculator.js
+++ src/temperatureApp/Calculator.js
@@ -1,24 +1,46 @@
 import React, { Component } from "react";
 import BoilingVerdict from "./BoilingVerdict";
+import TemperatureInput from "./TemperatureInput";
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { temperature: "" };
+    this.state = { temperature: 0, scale: "Celsius" };
   }
 
-  handleChange(e) {
-    this.setState({ temperature: e.target.value });
+  handleChange(e, scale) {
+    this.setState({ temperature: e.target.value, scale: scale });
   }
 
   render() {
     const temperature = this.state.temperature;
+    const scale = this.state.scale;
+    let fTemp;
+    let cTemp;
+    if (scale === "Celsius") {
+      // Convert celsius to fahrenheit
+      fTemp = Math.round((temperature * 9) / 5 + 32);
+      cTemp = temperature;
+    } else if (scale === "Fahrenheit") {
+      // Convert fahrenheit to celsius
+      cTemp = Math.round(((temperature - 32) * 5) / 9);
+      fTemp = temperature;
+    }
+
     return (
       <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-        <input value={temperature} onChange={this.handleChange} />
-        <BoilingVerdict celsius={parseFloat(temperature)} />
+        <TemperatureInput
+          scale="Fahrenheit"
+          handleChange={this.handleChange}
+          temperature={fTemp}
+        />
+        <TemperatureInput
+          scale="Celsius"
+          handleChange={this.handleChange}
+          temperature={cTemp}
+        />
+        <BoilingVerdict celsius={parseFloat(cTemp)} />
       </fieldset>
     );
   }
---
+++ src/temperatureApp/TemperatureInput.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class TemperatureInput extends Component {
+  render() {
+    const temperature = this.props.temperature;
+    return (
+      <div>
+        <legend>Enter temperature in {this.props.scale}:</legend>
+        <input
+          value={temperature}
+          onChange={(e) => {
+            this.props.handleChange(e, this.props.scale);
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default TemperatureInput;
